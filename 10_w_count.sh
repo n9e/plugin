bf71bb548bda3aa7970b28d5c9d5f14b3be4abbd
root <root@localhost.localdomain>
2020-05-15 17:42:31 +0800

    获取目前服务器登录用户数

---
+++ 10_w_count.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Activities=$(w |awk -F ':' '{if(NR>2) print $1}' |wc -l)
+
+localip=$(/usr/sbin/ifconfig `/usr/sbin/route|grep '^default'|awk '{print $NF}'`|grep inet|awk '{print $2}'|head -n 1)
+step=$(basename $0|awk -F'_' '{print $1}')
+timestamp=$(date +%s)
+echo '[
+    {
+        "endpoint": "'${localip}'",
+        "tags": "",
+        "timestamp": '$(timestamp)',
+        "metric": "net.w.count",
+        "value": '${Activities}',
+        "step": '${step}'
+    }
+]'
