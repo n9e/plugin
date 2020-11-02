136c3af43fa3a1d00600872b000f2b43aafbd622
freedomkk-qfeng <freedomkk_qfeng@qq.com>
2020-11-02 14:04:11 +0800

    修改：      pdns-cache-hit.py 	修改：      pdns-monitor.py 	新文件：    pdns-recursor-cache-hit.py 	重命名：    pdns-monitor.py -> pdns-recursor-monitor.py

--- powerdns/pdns-monitor.py
+++ powerdns/pdns-monitor.py
@@ -8,36 +8,39 @@ import commands
 
 ts = int(time.time())
 payload = []
-data = {"endpoint":"","metric":"","timestamp":ts,"value":0,"counterType":"COUNTER","tags":""}
+data = {"endpoint":"","metric":"","timestamp":ts,"step":60,"value":0,"counterType":"COUNTER","tags":""}
 
 gauge_metrics = [
-"tcp-clients",
 "fd-usage",
-"throttle-entries",
-"nsspeeds-entries",
-"cache-entries",
-"concurrent-queries",
-"max-mthread-stack",
-"packetcache-entries",
-"negcache-entries",
-"qa-latency",
-"uptime",
-"x-our-latency",
-"failed-host-entries",
+"key-cache-size",
+"latency",
+"meta-cache-size",
+"packetcache-size",
+"query-cache-size",
+"qsize-q",
+"real-memory-usage",
+"recursing-answers",
+"recursing-questions",
+"recursion-unanswered",
+"security-status",
+"signature-cache-size",
+"signatures",
+"uptime"
 ]
 
-a,pdns_stat = commands.getstatusoutput('rec_control get-all')
-pdns_array = pdns_stat.split("\n")
+a,pdns_stat = commands.getstatusoutput('pdns_control list')
+pdns_array = pdns_stat.split(",")
 for stat in pdns_array:
-	pdns = stat.split("\t")
-	metric = pdns[0]
-	value = pdns[1]
-	data["metric"] = "pdns-recursor." + metric
-	data["value"] = int(value)
-	if metric in gauge_metrics:
+	if stat == "":
+		continue
+        pdns = stat.split("=")
+        metric = pdns[0]
+        value = pdns[1]
+        data["metric"] = "pdns." + metric
+        data["value"] = int(value)
+        if metric in gauge_metrics:
             data["counterType"] = "GAUGE"
-	else:
+        else:
             data["counterType"] = "COUNTER"
-	payload.append(copy.copy(data))
-
+        payload.append(copy.copy(data))
 print json.dumps(payload,indent=4)
